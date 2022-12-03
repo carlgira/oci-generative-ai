b0cd22e45430ae0b68189eb4beeb3ca225feef47
carlgira@gmail.com <carlgira@gmail.com>
2022-12-03 18:23:35 +0100

    Fix sdv1.5

--- setup-instance.sh
+++ setup-instance.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-apt-get update -y
-apt-get upgrade -y
-apt install nvidia-driver-515 nvidia-dkms-515 nvidia-utils-515 -y
+main_function() {
+USER='ubuntu'
+apt update -y
 apt install wget git git-lfs python3 python3-pip python3-venv unzip -y
-apt-get install ffmpeg libsm6 libxext6 p7zip-full rapidjson-dev libarchive-dev zlib1g-dev -y
+apt install ffmpeg libsm6 libxext6 p7zip-full rapidjson-dev libarchive-dev zlib1g-dev -y
 
 # Install cuda
 wget -O /etc/apt/preferences.d/cuda-repository-pin-600 https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
 apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/3bf863cc.pub
 add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/ /" -y
-apt-get update -y
+apt update -y
 apt install cuda -y
-sudo -c "echo 'export PATH=/usr/local/cuda/bin:$PATH' >> ~/.bashrc" ubuntu
+sudo -c "echo 'export PATH=/usr/local/cuda/bin:$PATH' >> ~/.bashrc" $USER
 
 # Stable diffusion service
 cat <<EOT >> /etc/systemd/system/stable-diffusion.service
@@ -20,14 +20,15 @@ cat <<EOT >> /etc/systemd/system/stable-diffusion.service
 Description=systemd service start stable-diffusion
 
 [Service]
-ExecStart=/bin/bash /home/ubuntu/stable-diffusion-webui/webui.sh
-User=ubuntu
+ExecStart=/bin/bash /home/$USER/stable-diffusion-webui/webui.sh
+User=$USER
 
 [Install]
 WantedBy=multi-user.target
 EOT
 
-su -c 'git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git /home/ubuntu/stable-diffusion-webui' ubuntu
+su -c "git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git /home/$USER/stable-diffusion-webui" $USER
+su -c "cd /home/$USER/stable-diffusion-webui; git checkout 9bbe1e3" $USER
 
 # Bloom service
 cat <<EOT >> /etc/systemd/system/bloom.service
@@ -35,14 +36,15 @@ cat <<EOT >> /etc/systemd/system/bloom.service
 Description=systemd service start bloom
 
 [Service]
-ExecStart=/bin/bash /home/ubuntu/bloom-webui/start.sh
-User=ubuntu
+ExecStart=/bin/bash /home/$USER/bloom-webui/start.sh
+User=$USER
 
 [Install]
 WantedBy=multi-user.target
 EOT
 
-su -c 'git clone https://github.com/carlgira/bloom-webui.git /home/ubuntu/bloom-webui' ubuntu
+su -c "git clone https://github.com/carlgira/bloom-webui.git /home/$USER/bloom-webui" $USER
+su -c "cd /home/$USER/bloom-webui; git checkout e0d1a27" $USER
 
 # Dreambooth service
 
@@ -51,18 +53,22 @@ cat <<EOT >> /etc/systemd/system/dreambooth.service
 Description=systemd service start dreambooth
 
 [Service]
-ExecStart=/bin/bash /home/ubuntu/dreambooth-webui/start.sh
-User=ubuntu
+ExecStart=/bin/bash /home/$USER/dreambooth-webui/start.sh
+User=$USER
 
 [Install]
 WantedBy=multi-user.target
 EOT
 
-su -c 'git clone https://github.com/carlgira/dreambooth-webui.git /home/ubuntu/dreambooth-webui' ubuntu
+su -c "git clone https://github.com/carlgira/dreambooth-webui.git /home/$USER/dreambooth-webui" $USER
+su -c "cd /home/$USER/dreambooth-webui; git checkout 4a4a354" $USER
+
 
 systemctl daemon-reload
-systemctl enable stabble-diffusion.service
+systemctl enable stable-diffusion.service
 systemctl enable bloom.service
 systemctl enable dreambooth.service
+systemctl start stable-diffusion.service bloom.service dreambooth.service bloom.service
+}
 
-reboot
+main_function 2>&1 >> /var/log/startup.log
