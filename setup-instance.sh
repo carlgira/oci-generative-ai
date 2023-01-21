fb311403e2d73cce1f9f0f680adb404fab3224d2
oraclespainpresales <oraclespainpresales@gmail.com>
2023-01-22 00:43:00 +0100

    adding automatic-image-processing, and extenions on automatic1111

--- setup-instance.sh
+++ setup-instance.sh
@@ -20,7 +20,7 @@ cat <<EOT >> /etc/systemd/system/stable-diffusion.service
 Description=systemd service start stable-diffusion
 
 [Service]
-ExecStart=/bin/bash /home/$USER/stable-diffusion-webui/webui.sh
+ExecStart=/bin/bash /home/$USER/stable-diffusion-webui/webui.sh --api --xformers
 User=$USER
 
 [Install]
@@ -29,6 +29,16 @@ EOT
 
 su -c "git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git /home/$USER/stable-diffusion-webui" $USER
 su -c "cd /home/$USER/stable-diffusion-webui; git checkout 9bbe1e3" $USER
+
+# Remove background extension
+su -c "git clone https://github.com/KutsuyaYuki/ABG_extension /home/$USER/stable-diffusion-webui/extensions/ABG_extension" $USER
+# OutPaint extension
+su -c "git clone https://github.com/zero01101/openOutpaint-webUI-extension /home/$USER/stable-diffusion-webui/extensions/openOutpaint-webUI-extension" $USER
+# Deforum extension
+su -c "git clone https://github.com/deforum-art/deforum-for-automatic1111-webui /home/$USER/stable-diffusion-webui/extensions/deforum-for-automatic1111-webui" $USER
+
+# Add xformers
+su -c "echo https://github.com/C43H66N12O12S2/stable-diffusion-webui/releases/download/linux/xformers-0.0.14.dev0-cp310-cp310-linux_x86_64.whl >> /home/$USER/stable-diffusion-webui/requirements.txt" $USER
 
 # Bloom service
 cat <<EOT >> /etc/systemd/system/bloom.service
@@ -63,11 +73,25 @@ EOT
 su -c "git clone https://github.com/carlgira/dreambooth-webui.git /home/$USER/dreambooth-webui" $USER
 su -c "cd /home/$USER/dreambooth-webui; git checkout f21a51d" $USER
 
+# Smart Image processing service
+
+cat <<EOT >> /etc/systemd/system/automatic-image-processing.service
+[Unit]
+Description=systemd service start automatic-image-processing
+
+[Service]
+ExecStart=/bin/bash /home/$USER/automatic-image-processing/start.sh
+User=$USER
+
+[Install]
+WantedBy=multi-user.target
+EOT
+
+su -c "git clone https://github.com/carlgira/automatic-image-processing /home/$USER/automatic-image-processing" $USER
+
 systemctl daemon-reload
-systemctl enable stable-diffusion.service
-systemctl enable bloom.service
-systemctl enable dreambooth.service
-systemctl start stable-diffusion.service bloom.service dreambooth.service bloom.service
+systemctl enable stable-diffusion.service bloom.service dreambooth.service automatic-image-processing.service
+systemctl start stable-diffusion.service bloom.service dreambooth.service bloom.service automatic-image-processing.service
 }
 
 main_function 2>&1 >> /var/log/startup.log
