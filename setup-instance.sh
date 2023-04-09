ec3b2b96d87c44c70aed43be89faaa1710ccc9c9
carlgira <carlgira@gmail.com>
2023-04-09 10:31:08 +0200

    Adding stable-diffusion-webui commit

--- setup-instance.sh
+++ setup-instance.sh
@@ -14,7 +14,7 @@ vgs
 lvextend -l +100%FREE /dev/mapper/ocivolume-root
 xfs_growfs -d /
 
-sudo dnf install wget git python3.9 python39-devel.x86_64 libsndfile rustc cargo unzip zip git git-lfs -y
+dnf install wget git python3.9 python39-devel.x86_64 libsndfile rustc cargo unzip zip git git-lfs -y
 
 # Install ffmpeg
 dnf -y install https://download.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
@@ -39,6 +39,8 @@ WantedBy=multi-user.target
 EOT
 
 su -c "git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git /home/$USER/stable-diffusion-webui" $USER
+su -c "cd /home/$USER/stable-diffusion-webui; git checkout 2f01814" $USER # Working stable diffusion version (update if broken)
+
 
 # Add xformers
 su -c "echo https://github.com/C43H66N12O12S2/stable-diffusion-webui/releases/download/linux/xformers-0.0.14.dev0-cp310-cp310-linux_x86_64.whl >> /home/$USER/stable-diffusion-webui/requirements.txt" $USER
@@ -78,9 +80,29 @@ EOT
 
 su -c "git clone https://github.com/carlgira/dreambooth-webui.git /home/$USER/dreambooth-webui" $USER
 
+
+# Smart crop service
+
+cat <<EOT >> /etc/systemd/system/smart-crop.service
+[Unit]
+Description=systemd service smart-crop
+
+[Service]
+Environment="python_cmd=python3.9"
+Environment="pip_cmd=pip"
+ExecStart=/bin/bash /home/$USER/smart-crop/start.sh
+User=$USER
+
+[Install]
+WantedBy=multi-user.target
+EOT
+
+su -c "git clone https://github.com/carlgira/smart-crop.git /home/$USER/smart-crop" $USER
+
+
 systemctl daemon-reload
-systemctl enable stable-diffusion.service bloom.service dreambooth.service
-systemctl start stable-diffusion.service bloom.service dreambooth.service bloom.service
+systemctl enable stable-diffusion.service bloom.service dreambooth.service smart-crop.service
+systemctl start stable-diffusion.service bloom.service dreambooth.service bloom.service smart-crop.service
 }
 
 main_function 2>&1 >> /var/log/startup.log
